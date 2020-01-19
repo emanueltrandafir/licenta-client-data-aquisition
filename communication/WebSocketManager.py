60518f319bcccdc56eb27409851bf8f622880157
Emanuel Trandafir <emanuel.trandafir@pss.ro>
2020-01-19 15:30:13 +0200

    updated stomp client

--- communication/WebSocketManager.py
+++ communication/WebSocketManager.py
@@ -1,25 +1,32 @@
-# WS client example
 
-import asyncio
-import websockets
+import time
+import stomp
 
-async def hello():
-    uri = "ws://localhost:8080"
-    async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
 
-        await websocket.send(name)
-        print(f"> {name}")
+class MyListener(stomp.ConnectionListener):
+    def on_message(self, headers, message):
+        print('MyListener:\nreceived a message "{}"\n'.format(message))
+        global read_messages
+        read_messages.append({'id': headers['message-id'], 'subscription':headers['subscription']})
 
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
 
-asyncio.get_event_loop().run_until_complete(hello())
+class MyStatsListener(stomp.StatsListener):
+    def on_disconnected(self):
+        super(MyStatsListener, self).on_disconnected()
+        print('MyStatsListener:\n{}\n'.format(self))
 
-# import socket
-#
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind(('localhost',8090))
-#
-# s.send("gdgdgf")
 
+read_messages = []
+hosts = [('localhost', 8090)]
+
+conn = stomp.Connection(host_and_ports=hosts)
+conn.set_listener('my_listener', MyListener())
+conn.set_listener('stats_listener', MyStatsListener())
+# conn.start()
+
+conn.connect(wait=True)
+
+
+conn.subscribe(destination='/updates', id=1, ack='client-individual')
+conn.send(body="A Test message", destination='/updates')
+time.sleep(3)
